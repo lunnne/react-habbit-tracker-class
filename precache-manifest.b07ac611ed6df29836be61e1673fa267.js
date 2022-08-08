562ea48ab3fdf1a068cd64ccace80f3c29ba330a
Your Name <you@example.com>
2022-08-09 00:42:04 +0200

    Updates

---
+++ precache-manifest.b07ac611ed6df29836be61e1673fa267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65f56dba312c257b6cc7",
+    "url": "/react-habit-tracker-class/static/css/main.a47ceaf2.chunk.css"
+  },
+  {
+    "revision": "65f56dba312c257b6cc7",
+    "url": "/react-habit-tracker-class/static/js/main.65f56dba.chunk.js"
+  },
+  {
+    "revision": "0997fdd00010658ac3c9",
+    "url": "/react-habit-tracker-class/static/js/1.0997fdd0.chunk.js"
+  },
+  {
+    "revision": "c22a9c655f0cb739c11b",
+    "url": "/react-habit-tracker-class/static/js/runtime~main.c22a9c65.js"
+  },
+  {
+    "revision": "2591f466e74362d132c3e5c84fd08880",
+    "url": "/react-habit-tracker-class/index.html"
+  }
+];
